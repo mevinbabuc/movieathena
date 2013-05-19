874d334c83fd972a180cb5933e366e093c5318a7
Mevin <mevinbabuc@gmail.com>
2013-05-19 11:39:14 +0530

    url structure and functions defined

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
  
 app = Flask(__name__)      
  
+####
+##### App structure
+####
+
 @app.route('/')
 def landingPage():
     return render_template('landingPage.html')
@@ -12,9 +16,38 @@ def show_user_profile(username=None):
     return render_template('user.html',username=username)
 
 @app.route('/<moviename>')
-def show_post(moviename):
+def show_movie(moviename):
     return 'Movie %s' % moviename
+
+@app.route('/<moviename>/about')
+def show_movie(moviename):
+    return 'Movie %s analytics, cast and other details' % moviename
+
+@app.route('/Browse')
+def browse_movies():
+    return ' Browse movies'
+
+@app.route('/Browse/<category>')
+def browse_movies_category(category):
+    return ' Browse %s movies here' % category
  
+
+####
+##### Form submit urls
+####
+
+@app.route('/login')
+def login(category):
+    return ' Browse %s movies here' % category
+
+@app.route('/signup>')
+def sign_up(category):
+    return ' Browse %s movies here' % category
+
+####
+##### Error handlers
+####
+
 @app.errorhandler(404)
 def not_found(error):
     return "Better luck next time :P", 404
