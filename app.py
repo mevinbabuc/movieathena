79527ade7d274e1ef1dff0e799a5c19dc24a095e
Mevin <mevinbabuc@gmail.com>
2013-05-19 10:32:03 +0530

    added error handler 404

--- app.py
+++ app.py
@@ -11,11 +11,15 @@ def show_user_profile(username=None):
     # show the user profile for that user
     return 'User %s' % username
 
-@app.route('/shortFilm/<moviename>')
+@app.route('/<moviename>')
 def show_post(moviename):
     return 'Movie %s' % moviename
  
- 
+@app.errorhandler(404)
+def not_found(error):
+    return "Better luck next time :P", 404
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=8123)
