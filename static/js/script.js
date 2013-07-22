b432fe459e9ffe7f42ededc3db56decba4e3c4ae
Achu Eapen Alex <achuepnlx@gmail.com>
2013-07-22 12:08:11 +0530

    fb

--- static/js/script.js
+++ static/js/script.js
@@ -2,15 +2,3 @@
  *Author : Anenth
  */
 
-// Login/signup function
-$(document).ready(function() {
-  $.ajaxSetup({ cache: true });
-  $.getScript('//connect.facebook.net/en_UK/all.js', function(){
-    FB.init({
-      appId: '173786519447935',
-      channelUrl: 'http://127.0.0.1:8123/',
-    });     
-    $('#loginbutton,#feedbutton').removeAttr('disabled');
-    FB.getLoginStatus(updateStatusCallback);
-  });
-});
